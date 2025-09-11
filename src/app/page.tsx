dc3c77a8b1426e1bbd0b3f88770287d544e06de1
Tomas Gonzalez <tomas.gonzalez@example.com>
2025-09-12 09:10:35 +1200

    Remove EnvTest component and related test files; disable exhaustive-deps rule in ESLint configuration. Simplify Home component in page.tsx to only render SessionCreation.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,5 @@
 import SessionCreation from '@/components/SessionCreation'
-import EnvTest from '@/components/EnvTest'
 
 export default function Home() {
-  return (
-    <div>
-      <div className="p-4">
-        <EnvTest />
-      </div>
-      <SessionCreation />
-    </div>
-  )
+  return <SessionCreation />
 }
